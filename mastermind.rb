51cc579e52c0660931a248a29fde17fb4af53198
Bart Zweigoron <bzweigoron@gmail.com>
2022-12-08 11:06:44 -0500

    refactored code check logic

--- mastermind.rb
+++ mastermind.rb
@@ -1,37 +1,94 @@
-class Board
-  attr_accessor :round_number
-  attr_reader :secret_code
+class Mastermind
+  attr_accessor :round_number, :valid_guess_entered
+  attr_reader :secret_code, :possible_numbers, :number_of_rounds, :size_of_code, :blanks_allowed, :duplicates_allowed
 
-  def initialize(number_of_colors, size_of_code, number_of_rounds)
-    @number_of_colors = number_of_colors
+  def initialize(possible_numbers, size_of_code, number_of_rounds)
+    @possible_numbers = possible_numbers
     @number_of_rounds = number_of_rounds
     @size_of_code = size_of_code
     @round_number = 1
+    @blanks_allowed = false
+    @duplicates_allowed = false
+    @code_entry_error_msg = ''
+    @valid_guess_entered = false
   end
 
-  def set_custom_secret_code(code)
+
+  def generete_custom_secret_code(code)
     @secret_code = code.to_s.split('')
     p @secret_code
   end
 
-  def set_random_secret_code()
+  def generete_random_secret_code()
     @secret_code = create_secret_code
     p @secret_code
   end
 
-  private
-  def create_secret_code
-    code_array = []
-    for i in 1..@number_of_colors do
-      code_array << i.to_s
-    end
-    code_array = code_array.shuffle.slice(0,@size_of_code)
-
+  def enter_code
+    gets.chomp.split('')
   end
 
+  def validate_code(code)
+    @code_entry_error_msg = ''
+    @code_entry_error_msg += check_code_valid_numbers(code).to_s
+    @code_entry_error_msg += check_code_numeric(code).to_s
+    @code_entry_error_msg += check_code_length(code).to_s
+    @code_entry_error_msg
+  end
+
+  private
+
+  def create_secret_code
+    code_array = []
+    for i in 1..@possible_numbers do
+      code_array << i.to_s
+    end
+    code_array.shuffle.slice(0,@size_of_code)
+  end
+
+  def check_code_valid_numbers(code)
+    if code.min.to_i < 1 || code.max.to_i > @possible_numbers
+      @valid_guess_entered = false
+      "Out of range numbers entered. \n"
+    end
+  end
+
+  def check_code_numeric(code)
+    unless code.join('').to_i.to_s == code.join('').to_s
+      @valid_guess_entered = false
+      "Non-numeric charcaters entered. \n"
+    end
+  end
+
+  def check_code_length(code)
+    unless code.length == @size_of_code
+      @valid_guess_entered = false
+      "Code length must be #{@size_of_code} \n"
+    end
+  end
   # end class Board
 end
 
 # Begin Main Program
-board = Board.new(6,4,11) # number of colors / size of code / number of rounds
-board.set_random_secret_code
+game = Mastermind.new(6,4,11) # number of colors / size of code / number of rounds
+game.generete_random_secret_code
+
+puts ''
+puts 'Welcome to Mastermind. The rules are as follows.'
+puts '------------------------------------------------'
+puts "Allowed numbers: 1 thru #{game.possible_numbers}."
+puts "Size of code: #{game.size_of_code}."
+puts "Number of rounds: #{game.number_of_rounds}"
+puts "Blanks allowed: #{game.blanks_allowed}"
+puts "Duplicate numbers: #{game.duplicates_allowed}"
+puts ''
+# Begin round
+until game.valid_guess_entered
+  game.valid_guess_entered = true # will be set to false if game.validate_code fails
+  puts "Round #{game.round_number}: Enter your guess."
+  guess = game.enter_code
+  puts "\nYou entered #{guess}"
+  valid_code_msg = game.validate_code(guess)
+  valid_code_msg += "Only enter numbers 1 thru #{game.possible_numbers}. \n\n" unless game.valid_guess_entered
+  puts valid_code_msg
+end
